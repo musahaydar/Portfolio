f36eb30a2aec910ac5928fa2d25640964032e9b5
Musa <musah@umich.edu>
2021-10-14 16:02:11 -0400

    Switched to using JSON to load page content

--- generate.py
+++ generate.py
@@ -1,23 +1,12 @@
 # Simple script to generate pages for projects
 from jinja2 import Template
+import json
 
 def generateProjectPages():
     with open('templates/project.html') as file:
         template = Template(file.read())
     
-    contexts = [
-        {
-            'file': 'portfolio',
-            'name': 'Portfolio Website',
-            'body': 'I finally updated my portfolio website to have a list of projects. Currently, it\'s a static website, with pages for each project generated using Python and Jinja.',
-            'links': [
-                {
-                    "url":"https://github.com/musahaydar/Portfolio",
-                    "text":'[0] view source code on GitHub'
-                }
-            ]
-        } 
-    ]
+    contexts = json.load(open('projects.json'))
 
     for context in contexts:
         with open('html/projects/' + context['file'] + '.html', 'w') as output:
